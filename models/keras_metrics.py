c5a79206180b9d88182433118c60d534cc4225bc
felipessalvatore <felipessalvador@egooglemail.com>
2017-06-01 12:56:52 -0300

    Add pep8 style

--- models/keras_metrics.py
+++ models/keras_metrics.py
@@ -2,37 +2,63 @@
 # -*- coding: utf-8 -*-
 
 from keras import backend as K
-import numpy as np
+
 
 class KerasMetrics:
-	def precision(self, y_true, y_pred):
-		true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-		predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-		precision = true_positives / (predicted_positives + K.epsilon())
-		return precision
+    def precision(self, y_true, y_pred):
+        '''
+        calculates precision
 
+        :type y_true: np array
+        :type y_pred: np array
+        :rtype: float
+        '''
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
 
-	def recall(self, y_true, y_pred):
-		true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-		possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-		recall = true_positives / (possible_positives + K.epsilon())
-		return recall
+    def recall(self, y_true, y_pred):
+        '''
+        calculates recall
 
+        :type y_true: np array
+        :type y_pred: np array
+        :rtype: float
+        '''
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
 
-	def fbeta_score(self, y_true, y_pred, beta=1):
-		if beta < 0:
-			raise ValueError('The lowest choosable beta is zero (only precision).')
+    def fbeta_score(self, y_true, y_pred, beta=1):
+        '''
+        calculates f beta score
 
-		if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
-			return 0
+        :type y_true: np array
+        :type y_pred: np array
+        :type beta: int
+        :rtype: float
+        '''
+        if beta < 0:
+            info = 'The lowest choosable beta is zero (only precision).'
+            raise ValueError(info)
 
-		p = self.precision(y_true, y_pred)
-		r = self.recall(y_true, y_pred)
-		bb = beta ** 2
-		fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
-		return fbeta_score
+        if K.sum(K.round(K.clip(y_true, 0, 1))) == 0:
+            return 0
 
+        p = self.precision(y_true, y_pred)
+        r = self.recall(y_true, y_pred)
+        bb = beta ** 2
+        fbeta_score = (1 + bb) * (p * r) / (bb * p + r + K.epsilon())
+        return fbeta_score
 
-	def f2_score(self, y_true, y_pred):
-		return self.fbeta_score(y_true, y_pred, beta=2)
+    def f2_score(self, y_true, y_pred):
+        '''
+        calculates f 2 score
 
+        :type y_true: np array
+        :type y_pred: np array
+        :rtype: float
+        '''
+        return self.fbeta_score(y_true, y_pred, beta=2)
