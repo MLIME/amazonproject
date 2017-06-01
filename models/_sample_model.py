c5c011d43f6648891fcaa0c89e8dafa14fe6a8a5
felipessalvatore <felipessalvador@egooglemail.com>
2017-06-01 12:11:09 -0300

    Hotfix: add pep8 style

--- models/_sample_model.py
+++ models/_sample_model.py
@@ -1,16 +1,16 @@
 from base_model import BaseModel
 
+
 class SampleModel(BaseModel):
     def __init__(self):
         self.args_names = ['--arg1', '--arg2']
         self.args_desc = ['arg1 desc', 'arg2 desc']
-    
+
     def args(self):
         '''
         Lists model parameters
         '''
         return [(k, v) for (k, v) in zip(self.args_names, self.args_desc)]
-
 
     def initialize(self, args):
         '''
@@ -18,13 +18,11 @@ class SampleModel(BaseModel):
         '''
         pass
 
-
     def fit(self, X_train, y_train, X_valid, y_valid):
         '''
         Fits a model
         '''
         pass
-
 
     def predict(self, X_test):
         '''
